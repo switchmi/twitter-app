0f57c875ebf36ee7a83efeb3d6ad3d84b1bec64c
fahmi <seichan83@gmail.com>
2017-10-27 03:22:37 +0800

    Basic twitter login and create functionality with css

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,7 +1,31 @@
 class TweetsController < BaseController
 
   def index
+    @tweets = current_user.tweets.all
+    @tweet = current_user.tweets.new
 
+  end
+
+  def create
+    @tweets = current_user.tweets.all
+    @tweet = current_user.tweets.new(get_params)
+    if @tweet.save
+      redirect_to tweets_path
+    else
+      render :index
+    end
+  end
+
+  def destroy
+    @tweet = current_user.tweets.find(params[:id])
+    @tweet.destroy
+    redirect_to tweets_path
+  end
+
+  private
+
+  def get_params
+    params.require(:tweet).permit(:text)
   end
 
 end
